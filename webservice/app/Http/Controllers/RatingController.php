38788ddff4d2f5c1459b75cd7e228c6c54077793
felixboenawan <felix.boenawan@gmail.com>
2018-07-08 19:34:43 +0700

    get rating data

--- webservice/app/Http/Controllers/RatingController.php
+++ webservice/app/Http/Controllers/RatingController.php
@@ -38,10 +38,13 @@ class RatingController extends Controller
         return response()->json(['data' => [], 'message' => ['Rating tidak boleh kosong']]);
     }
     else {
-
+        
         $user_id = $request->get('user')->id;
         $order = Order::find($request->order_id);
-
+        $rating = Rating::where('order_id', $request->order_id)->first();
+        if ($rating != null ){
+            return response()->json(['message' => ['Anda sudah memberikan rating untuk transaksi ini.']]);
+        }
         $data['order_id'] = $request->order_id;
         $data['agen_id'] = $order->agen_id;
         $data['customer_id'] = $user_id;
