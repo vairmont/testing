dec4f1de8b732f4c85c929e284239020c966e5ae
felixboenawan <felix.boenawan@gmail.com>
2018-07-08 19:40:30 +0700

    get rating data

--- webservice/app/Http/Controllers/RankingController.php
+++ webservice/app/Http/Controllers/RankingController.php
@@ -10,7 +10,7 @@ class RankingController extends Controller
 {
     public function index(Request $request) {
 
-          $items = Order::Join('agen', 'order.agen_id', '=', 'agen.id')
+          $items = Order::Join('agen', 'order.agen_id', '=', 'agen.identifier')
           ->where('order.status','7')
           ->select(DB::raw('SUM(total) as total_sales'), 'agen.name')
           ->groupBy('agen.name')
