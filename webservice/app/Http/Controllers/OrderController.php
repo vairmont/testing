7cbe71f9abbf70c6b37db2d568a3525fb336cc34
felixboenawan <felix.boenawan@gmail.com>
2018-07-08 19:04:33 +0700

    get rating data

--- webservice/app/Http/Controllers/OrderController.php
+++ webservice/app/Http/Controllers/OrderController.php
@@ -150,7 +150,6 @@ class OrderController extends Controller
       $result = [];
       foreach ($orders as $order) {
         $items = OrderDetail::Join('product', 'product.id', '=', 'order_detail.product_id')
-          ->where('rating.order_id', '=', $order->id)
           ->select('product.id as product_id', 'product.sku', 'product.product_name', 'order_detail.qty','product.price_for_customer','product.price_for_agen','product.img_url')
           ->get();
 
