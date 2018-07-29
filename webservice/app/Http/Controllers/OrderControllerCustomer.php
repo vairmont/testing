8f1064ab5c6b555efe3e170d5e226147ceacd68b
felixboenawan <felix.boenawan@gmail.com>
2018-07-29 09:46:30 +0700

    order done

--- webservice/app/Http/Controllers/OrderControllerCustomer.php
+++ webservice/app/Http/Controllers/OrderControllerCustomer.php
@@ -60,7 +60,6 @@ class OrderControllerCustomer extends Controller
 
       $orders = Order::join('order_billing_detail', 'order_billing_detail.order_id', '=', 'order.id')
       ->join('agen', 'agen.identifier', '=', 'order.agen_id')
-      ->where('order.agen_id', '=', 'agen.identifier')
       ->where('user_id', '=', $request->get('user')->id)
       ->where('status', '=', 7)
       ->select('order.*','order_billing_detail.customer_name','order_billing_detail.customer_phone','order_billing_detail.customer_address','order_billing_detail.lat','order_billing_detail.long','order_billing_detail.customer_address2', 'order_billing_detail.notes as order_notes', 'agen.name as agen_name', 'agen.photo as agen_photo')
