aced128de3fb0fd42ed73776d47770bc955451f4
felixboenawan <felix.boenawan@gmail.com>
2018-08-10 18:38:41 +0700

    ranks

--- webservice/app/Http/Controllers/RankingController.php
+++ webservice/app/Http/Controllers/RankingController.php
@@ -5,19 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use DB;
 use App\Order;
+use App\User;
+use App\Store;
 
 class RankingController extends Controller
 {
     public function index(Request $request) {
 
-          $items = Order::Join('agen', 'order.agen_id', '=', 'agen.identifier')
-          ->where('order.status','7')
-          ->select(DB::raw('SUM(total) as total_sales'), 'agen.name')
-          ->groupBy('agen.name')
-          ->orderBy('total_sales', 'desc')
-          ->get();
+          $store = User::Join('order', 'order.agen_id', '=', 'users.id')
+          		   ->join('agen', 'agen.identifier', '=', 'users.id')
+          		   ->join('store', 'store.id', '=', 'users.store_id')
+          		   ->where('order.status', '7')
+          		   ->select(DB::raw('SUM(total) as total_sales'), 'agen.name', 'store.store_name', 'agen.photo')
+          		   ->groupBy('agen.name','store.store_name','agen.photo')
+		           ->orderBy('total_sales', 'desc')
+		           ->get();
 
-      return response()->json($items, 200);
+      return response()->json($store, 200);
     }
 }
 
