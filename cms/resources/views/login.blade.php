2e6fcd6a24911a2c3a5e30f0c5c86d548238bd92
Bobandrean <bobandrean94@gmail.com>
2018-09-17 22:16:07 +0700

    pull

--- cms/resources/views/login.blade.php
+++ cms/resources/views/login.blade.php
@@ -9,24 +9,37 @@
 
     <title>Login</title>
 
+    <link  type="text/css" href="{{asset('vendor/animate/animate.css')}}" rel="stylesheet">
+
     <!-- Bootstrap Core CSS -->
     <link href="{{ asset('sbadmin/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
     <!-- MetisMenu CSS -->
-    <link href="{{ asset('sbadmin/vendor/metisMenu/metisMenu.min.cs') }}" rel="stylesheet">
-
+  
     <!-- Custom CSS -->
     <link href="{{ asset('sbadmin/dist/css/sb-admin-2.css') }}" rel="stylesheet">
+
+ 
+
 
     <!-- Custom Fonts -->
     <link href="{{ asset('sbadmin/vendor/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
 
+
+
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <style>
+    body {
+    background-color: coral;
+}
+    div{
+        background-color:red;
+    }
+    .login-panel{
+        background-color:blue;
+    }
+</style>
 
 </head>
 
@@ -34,37 +47,37 @@
 
     <div class="container">
         <div class="row">
-            <div class="col-md-4 col-md-offset-4">
+            <div class="panel-con col-md-4 col-md-offset-4">
                 <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Grosir One. Please sign in</h3>
-                    </div>
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Login</h3>
+                        </div>
                     <div class="panel-body">
          
-                        <form form class="form-horizontal" method="GET" action="{{url('/dologin')}}" >
-                        {{csrf_field()}}
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Phone" name="phone" type="text" >
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Password" name="password"  value="" type="password">
-                                </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                                    </label>
-                                </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <button type="submit" class="btn btn-primary">Login</button>
-                            </fieldset>
-                      
-                        </form>
+                            <form form class="form-horizontal" method="GET" action="{{url('/dologin')}}" >s 
+                            {{csrf_field()}}
+                                <fieldset>
+                                    <div class="form-group">
+                                        <input class="form-control" placeholder="Phone" name="phone" type="text" >
+                                    </div>
+                                    <div class="form-group">
+                                        <input class="form-control" placeholder="Password" name="password" value="" type="password">
+                                    </div>
+                                    <div class="checkbox">
+                                        <label>
+                                            <input name="remember" type="checkbox" value="Remember Me">Remember Me
+                                        </label>
+                                    </div>
+                                    <!-- Change this to a button or input when using this as a form -->
+                                    <button type="submit" class="btn btn-primary">Login</button>
+                                </fieldset>
+                            </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 
     <!-- jQuery -->
     <script src="../vendor/jquery/jquery.min.js"></script>
