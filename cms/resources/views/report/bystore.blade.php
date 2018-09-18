bd9a8ecd4d5b6ea35735a52da7e0afe00161a2fc
felixboenawan <felix.boenawan@gmail.com>
2018-09-18 10:35:42 +0700

    flow sales

--- cms/resources/views/report/bystore.blade.php
+++ cms/resources/views/report/bystore.blade.php
@@ -74,7 +74,7 @@
                                                   <td>{{$flow->invoice}}</td>
                                                   <td>{{$flow->proname}}</td>
                                                   <td>{{$flow->qty}}</td>
-                                                  <td>{{number_format((($flow->promo_price == 0) ? $flow->price_for_customer : $flow->promo_price) * $flow->qty * $this->marginRate)}}</td>
+                                                  <td>{{number_format((($flow->promo_price == 0) ? $flow->price_for_customer : $flow->promo_price) * $flow->qty * 0.05)}}</td>
                                                   <td>{{$flow->rate}}% (Rp {{ number_format($flow->agen_price * $flow->rate / 100) }})</td>
                                                   <td>{{number_format($flow->agen_price * $flow->qty)}}</td>
                                                   <td>{{number_format((($flow->promo_price == 0) ? $flow->price_for_customer : $flow->promo_price) * $flow->qty)}}</td>
