9301ea2a9605c8732fc6e99fd2bb1fbed17a9bf4
felixboenawan <felix.boenawan@gmail.com>
2018-11-16 11:37:31 +0700

    order customer

--- webservice/app/Http/Controllers/OrderControllerCustomer.php
+++ webservice/app/Http/Controllers/OrderControllerCustomer.php
@@ -36,7 +36,7 @@ class OrderControllerCustomer extends Controller
       ->join('order_billing_detail','order_billing_detail.order_id','=','order.id')
       ->where('user_id', '=', $request->get('user')->id)
       ->whereIn('status',[1,2,6])
-      ->select('order.*','customer.name as name','order_billing_detail.customer_name','order_billing_detail.customer_phone','order_billing_detail.customer_address','order_billing_detail.lat','order_billing_detail.long','order_billing_detail.customer_address2', 'order_billing_detail.notes as order_notes')
+      ->select('order.*','customer.name as name','order_billing_detail.customer_name','order_billing_detail.customer_phone','order_billing_detail.customer_address','order_billing_detail.customer_address2', 'order_billing_detail.notes as order_notes')
       ->orderBy('created_at', 'asc')
       ->get();
 
@@ -64,7 +64,7 @@ class OrderControllerCustomer extends Controller
       ->join('agen', 'agen.identifier', '=', 'order.agen_id')
       ->where('user_id', '=', $request->get('user')->id)
       ->where('status', '=', 7)
-      ->select('order.*', 'order_billing_detail.order_id', 'order_billing_detail.customer_name','order_billing_detail.customer_phone','order_billing_detail.customer_address','order_billing_detail.lat','order_billing_detail.long','order_billing_detail.customer_address2', 'order_billing_detail.notes as order_notes', 'agen.name as agen_name', 'agen.photo as agen_photo')
+      ->select('order.*', 'order_billing_detail.order_id', 'order_billing_detail.customer_name','order_billing_detail.customer_phone','order_billing_detail.customer_address','order_billing_detail.customer_address2', 'order_billing_detail.notes as order_notes', 'agen.name as agen_name', 'agen.photo as agen_photo')
       ->get();
 
       $result = [];
