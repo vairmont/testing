67b228bf5cb5189648b450a9e963ee5c6285b740
felixboenawan <felix.boenawan@gmail.com>
2018-11-28 11:26:58 +0700

    history order sembako

--- webservice/app/Http/Controllers/OrderControllerCustomer.php
+++ webservice/app/Http/Controllers/OrderControllerCustomer.php
@@ -508,7 +508,7 @@ class OrderControllerCustomer extends Controller
       foreach ($orders as $order) {
         $items = OrderDetail::Join('product', 'product.id', '=', 'order_detail.product_id')
           ->where('order_id', '=', $order->id)
-          ->select('product.id as product_id', 'product.sku', 'product.product_name', 'order_detail.qty','product.price_for_customer','product.price_for_agen','product.img_url')
+          ->select('product.id as product_id', 'product.sku', 'product.product_name', 'order_detail.qty','order_detail.price_for_customer','product.img_url')
           ->get();
 
         $result[] = [
